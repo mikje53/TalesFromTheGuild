ac38147f64c71830c94502eca8423e6bb8c2c157
mikje53 <im.mikkel@gmail.com>
2019-02-09 14:37:51 +0100

    Update GameManager and HandOfCards prefabs with scripts for initialization

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]
+    public int handSize;
+
+    [SerializeField]
+    public GameObject handOfCards;
+
+    private HandOfCards _handOfCards;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _handOfCards = handOfCards.GetComponent<HandOfCards>();
+        _handOfCards.initializeHand(handSize);
+    }
+}
---
+++ Assets/Scripts/HandOfCards.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandOfCards : MonoBehaviour
+{
+    [SerializeField]
+    public GameObject cardPrefab;
+
+    public int handSize;
+
+    private float cardSpaceBuffer = 0.1f;
+    
+    public void initializeHand(int initialHandSize) {
+        this.handSize = initialHandSize;
+        for(int i = 0; i < this.handSize; i++) {
+            Instantiate(cardPrefab, new Vector3(i - 1 + (cardSpaceBuffer*i), 0.1f + (i/10), -3), Quaternion.Euler(90, 0, 0));
+        }
+    }
+}
